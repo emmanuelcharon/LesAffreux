43f0e51ba5d68c2a7d750d005c61bded1b9408a5
Emmanuel Charon <emmanuelcharon@MacBook-Pro-de-Emmanuel-2.local>
2016-02-11 21:12:36 +0100

    ss

--- Hashcode_2016/root/first.py
+++ Hashcode_2016/root/first.py
@@ -4,27 +4,82 @@ Created on 11 fvr. 2016
 @author: emmanuelcharon
 '''
 import math
-from reader import Reader, Warehouse, Order
+from reader import Reader, Warehouse, Order, Load, Deliver
 
 def dist(r1, c1, r2, c2):
   return math.sqrt((r1-r2)*(r1-r2) + (c1-c2)(c1-c2))
 
-def Drone():
-  def __init__(self, reader):
+class Drone(object):
+  def __init__(self, reader, ID):
     self.R = reader.WAREHOUSES[0].R
     self.C = reader.WAREHOUSES[0].C
     self.items = [0]*reader.P # start with 0 items
     self.nextAvailableTime = 0 # available now
     self.commands = []
+    self.ID = ID
  
-#closest warehouse with an item of type i
+class Job(object):
+  def __init__(self, R, C, productType, orderID):
+    self.R = R
+    self.C = C
+    self.productType = productType
+    self.orderID = orderID
+    
+def availableWorkers(reader, drones, t):
+  result = []
+  for drone in drones:
+    if drone.nextAvailableTime <= t:
+      result.append(drone)
+  return result
+
+def doAllJobs(reader):
+  
+  sortedOrders = reader.ORDERS[:]
+  sortedOrders.sort(cmp=lambda x,y: cmp(x.weight, y.weight), reverse=True) 
+  
+  allJobs = []
+  for order in sortedOrders:
+    for item in order.rawItems:
+      job = Job(order.R, order.C, item, order.ID)
+      allJobs.append(job)
+  
+  drones = []
+  for _ in range(0, reader.D):
+    drone = Drone(reader, _)
+    drones.append(drone)
+  
+  commands = []
+    
+  for t in range(0, reader.T):
+    
+    avDrones = availableWorkers(reader, drones, t)
+    
+    for drone in avDrones:
+      job = allJobs[0]
+      
+      closestWHindex = closestWwithP(job.productType, reader, drone.R, drone.C)
+      wh = reader.WAREHOUSES[closestWHindex]
+      
+      t1 = dist(drone.R, drone.C, wh.R, wh.C) 
+      t2 = dist(wh.R, wh.C, job.R, job.C) 
+      jobTime = t1+t2+1
+      
+      if t + jobTime < reader.T:
+        allJobs.pop()
+        commands.append(Load(drone.ID, closestWHindex, job.productType, 1))
+        wh.ITEMS[job.productType] -= 1
+        commands.append(Deliver(drone.ID, job.orderID, job.productType, 1))
+        drone.nextAvailableTime += jobTime
+  return commands
+
+#closest warehouse with an item of type productType
 def closestWwithP(productType, reader, r, c):
   
   d = 1000*1000*1000
   closest = 0
   
-  for id in range(len(reader.WARHOUSES)):
-    wh = reader.WAREHOUSES[id]
+  for whID in range(len(reader.WARHOUSES)):
+    wh = reader.WAREHOUSES[whID]
     
     if wh.ITEMS[productType] > 0:
       d_wh = dist(wh.R, wh.C, r, c)
@@ -32,26 +87,6 @@ def closestWwithP(productType, reader, r, c):
         closest = id
   return closest
 
-def nextProductNeeded(order, itemsCarried, reader):
-  for i in range(0, reader.P):
-    if order.ITEMS[i]>0 and itemsCarried[i]<order[i]:
-      return i
-  return -1 # no product needed
-  
- 
-def solveOrder(reader, order):
-  drones = [] # a list of actions for each drone needed
-  
-  itemsCarried = [0]*reader.P
-  productType = nextProductNeeded(order, itemsCarried, reader)
-  
-  while(productType>=0):
-    pass
-    # new random available drone
-  # no items needed
-  return drones
-  
-  
   
   # find closest warehouse with item 0
   # then build the list of items this warehouse (still), this will be the last stop of the drone
@@ -65,23 +100,6 @@ def solveOrder(reader, order):
   # we choose a start time when all drones are available
   # remember for each drone the time when they get available  
   
-  pass
-
-def first(reader):
-  
-  sortedOrders = reader.ORDERS[:]
-  sortedOrders.sort(cmp=lambda x,y: cmp(x.weight, y.weight), reverse=True) 
-  drones = []
-  
-  for _ in range(reader.D):
-    drone = Drone(reader) 
-    
-    order = sortedOrders.pop(0)
-    itemsCarried = [0]*reader.P
-    
-    drones.append(drone)
-    
-  return drones
   
 
 if __name__ == '__main__':
